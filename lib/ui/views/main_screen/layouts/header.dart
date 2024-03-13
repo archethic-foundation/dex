037c9710d4da9bdf0e64ba7b0dd5d37997749506
redDwarf03 <sylvain@archethic.net>
2024-03-13 15:47:40 +0100

    💄 Change header

--- lib/ui/views/main_screen/layouts/header.dart
+++ lib/ui/views/main_screen/layouts/header.dart
@@ -22,138 +22,159 @@ class Header extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final indexMenu = ref.watch(navigationIndexMainScreenProvider);
 
-    return Row(
+    return Stack(
       children: [
-        const SizedBox(
-          width: 16,
-        ),
-        SvgPicture.asset(
-          'assets/images/AELogo.svg',
-          height: 34,
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 4),
+        Positioned(
+          top: 40,
+          left: 65,
           child: SelectableText(
-            'aeSwap',
+            'Decentralized Exchange',
             style: TextStyle(
-              fontSize: 33,
+              fontSize: 10.5,
               color: aedappfm.ArchethicThemeBase.neutral0,
             ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 5, bottom: 26),
-          child: SelectableText(
-            'Beta',
-            style: Theme.of(context).textTheme.labelMedium,
-          ),
-        ),
-        if (withMenu &&
-            aedappfm.Responsive.isMobile(context) == false &&
-            aedappfm.Responsive.isTablet(context) == false)
-          Row(
-            children: [
-              const SizedBox(
-                width: 50,
+        Row(
+          children: [
+            const SizedBox(
+              width: 16,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: SvgPicture.asset(
+                'assets/images/AELogo.svg',
+                height: 34,
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        context.go(SwapSheet.routerPage);
-                      },
-                      child: Text(
-                        AppLocalizations.of(context)!.menu_swap,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w300,
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 7),
+              child: SelectableText(
+                'aeSwap',
+                style: TextStyle(
+                  fontSize: 33,
+                  color: aedappfm.ArchethicThemeBase.neutral0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 5, bottom: 26),
+              child: SelectableText(
+                'Beta',
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+            ),
+            if (withMenu &&
+                aedappfm.Responsive.isMobile(context) == false &&
+                aedappfm.Responsive.isTablet(context) == false)
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 10, left: 20, right: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            context.go(SwapSheet.routerPage);
+                          },
+                          child: Text(
+                            AppLocalizations.of(context)!.menu_swap,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300,
+                              color: indexMenu == NavigationIndex.swap
+                                  ? aedappfm.ArchethicThemeBase.raspberry200
+                                  : aedappfm.ArchethicThemeBase.neutral0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 5),
+                          height: 0.5,
+                          width: 70,
                           color: indexMenu == NavigationIndex.swap
                               ? aedappfm.ArchethicThemeBase.raspberry200
-                              : aedappfm.ArchethicThemeBase.neutral0,
+                              : Colors.transparent,
                         ),
-                      ),
+                      ],
                     ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 5),
-                      height: 0.5,
-                      width: 70,
-                      color: indexMenu == NavigationIndex.swap
-                          ? aedappfm.ArchethicThemeBase.raspberry200
-                          : Colors.transparent,
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        context.go(PoolListSheet.routerPage);
-                      },
-                      child: Text(
-                        AppLocalizations.of(context)!.menu_liquidity,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w300,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 10, left: 20, right: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            context.go(PoolListSheet.routerPage);
+                          },
+                          child: Text(
+                            AppLocalizations.of(context)!.menu_liquidity,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300,
+                              color: indexMenu == NavigationIndex.pool
+                                  ? aedappfm.ArchethicThemeBase.raspberry200
+                                  : aedappfm.ArchethicThemeBase.neutral0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 5),
+                          height: 0.5,
+                          width: 90,
                           color: indexMenu == NavigationIndex.pool
                               ? aedappfm.ArchethicThemeBase.raspberry200
-                              : aedappfm.ArchethicThemeBase.neutral0,
+                              : Colors.transparent,
                         ),
-                      ),
+                      ],
                     ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 5),
-                      height: 0.5,
-                      width: 90,
-                      color: indexMenu == NavigationIndex.pool
-                          ? aedappfm.ArchethicThemeBase.raspberry200
-                          : Colors.transparent,
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        context.go(FarmListSheet.routerPage);
-                      },
-                      child: Text(
-                        AppLocalizations.of(context)!.menu_farm,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w300,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 10, left: 20, right: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            context.go(FarmListSheet.routerPage);
+                          },
+                          child: Text(
+                            AppLocalizations.of(context)!.menu_farm,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300,
+                              color: indexMenu == NavigationIndex.farm
+                                  ? aedappfm.ArchethicThemeBase.raspberry200
+                                  : aedappfm.ArchethicThemeBase.neutral0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 5),
+                          height: 0.5,
+                          width: 70,
                           color: indexMenu == NavigationIndex.farm
                               ? aedappfm.ArchethicThemeBase.raspberry200
-                              : aedappfm.ArchethicThemeBase.neutral0,
+                              : Colors.transparent,
                         ),
-                      ),
+                      ],
                     ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 5),
-                      height: 0.5,
-                      width: 70,
-                      color: indexMenu == NavigationIndex.farm
-                          ? aedappfm.ArchethicThemeBase.raspberry200
-                          : Colors.transparent,
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+          ],
+        ),
       ],
     );
   }
