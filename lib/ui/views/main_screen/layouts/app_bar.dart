160a6489547cb2c2a2574440dc830f0a6ce1fd11
redDwarf03 <sylvain@archethic.net>
2024-04-18 16:31:25 +0200

    💄 Hide header in mobile mode

--- lib/ui/views/main_screen/layouts/app_bar.dart
+++ lib/ui/views/main_screen/layouts/app_bar.dart
@@ -2,6 +2,8 @@ import 'package:aedex/ui/views/main_screen/layouts/app_bar_menu_info.dart';
 import 'package:aedex/ui/views/main_screen/layouts/app_bar_menu_links.dart';
 import 'package:aedex/ui/views/main_screen/layouts/connection_to_wallet_status.dart';
 import 'package:aedex/ui/views/main_screen/layouts/header.dart';
+import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart'
+    as aedappfm;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -29,6 +31,8 @@ class _AppBarMainScreenState extends ConsumerState<AppBarMainScreen> {
   );
   @override
   Widget build(BuildContext context) {
+    if (aedappfm.Responsive.isMobile(context)) return const SizedBox.shrink();
+
     return Padding(
       padding: const EdgeInsets.only(top: 8),
       child: AppBar(
