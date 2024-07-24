017fe7043e0625c64e3b486bc9a2302fa1561c37
redDwarf03 <sylvain@archethic.net>
2024-07-24 18:44:04 +0200

    feat: :sparkles: Add information about farming

--- lib/ui/views/util/consent_uri.dart
+++ lib/ui/views/util/consent_uri.dart
@@ -1,3 +1,5 @@
 const kURIPrivacyPolicy =
     'https://www.archethic.net/privacy-policy-archethic.html';
 const kURITermsOfUse = 'https://www.archethic.net/terms-of-use-defi.html';
+const kURIFarmLockArticle =
+    'https://medium.com/@archethic.social/aeswap-update-new-farming-mechanism-8ecf3540a6ae';
