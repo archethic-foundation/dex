53781a76c0296d0f9d2634c43e0f9ccea25823bc
redDwarf03 <sylvain@archethic.net>
2024-07-25 17:17:51 +0200

    Fix null exception

--- lib/ui/views/pool_list/layouts/components/pool_details_info_apr_farm.dart
+++ lib/ui/views/pool_list/layouts/components/pool_details_info_apr_farm.dart
@@ -46,7 +46,10 @@ class PoolDetailsInfoAPRFarmState
           ).future,
         );
         setState(() {
-          apr3years = farmLock!.stats['7']?.aprEstimation ?? 0;
+          // TODO: Manage < 3 years
+          if (farmLock != null && farmLock.stats['7'] != null) {
+            apr3years = farmLock.stats['7']?.aprEstimation ?? 0;
+          }
         });
       }
     });
