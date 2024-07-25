13ac584fc8e7c5bb5effdd1ba134bd6e1c04b294
redDwarf03 <sylvain@archethic.net>
2024-07-25 19:33:20 +0200

    Add Farm address mainnet

--- lib/ui/views/util/components/pool_farm_available.dart
+++ lib/ui/views/util/components/pool_farm_available.dart
@@ -130,7 +130,8 @@ class PoolFarmAvailableState extends State<PoolFarmAvailable> {
               '000090C5AFCC97C2357E964E3DDF5BE9948477F7C1DE2C633CDFC95B202970AEA036',
           aeETHUCOFarmLegacyAddress:
               '0000474A5B5D261A86D1EB2B054C8E7D9347767C3977F5FC20BB8A05D6F6AFB53DCC',
-          aeETHUCOFarmLockAddress: ''
+          aeETHUCOFarmLockAddress:
+              '0000b2339aadf5685b1c8d400c9092c921e51588dc049e097ec9437017e7dded0feb'
         );
     }
   }
