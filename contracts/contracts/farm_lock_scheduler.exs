c9472bda5c255ca473f97da23bf4236abb62db79
bchamagne <bastien@archethic.net>
2024-08-01 09:46:54 +0200

    feat(scheduler): close the contract once farm end

--- contracts/contracts/farm_lock_scheduler.exs
+++ contracts/contracts/farm_lock_scheduler.exs
@@ -1,5 +1,18 @@
 @version 1
 
+# allow code change
+condition inherit: [
+  code: true
+]
+
+actions triggered_by: datetime, at: 1848658200 do
+  # stop the scheduler 10 minutes after farm's end
+  Contract.set_type("data")
+  Contract.set_content("so long and thanks for all the fish")
+  Contract.set_code("")
+end
+
+# every hour trigger farm's "calculate_rewards" action
 actions triggered_by: interval, at: "5 * * * *" do
   Contract.add_recipient(
     address: 0x0000B2339AADF5685B1C8D400C9092C921E51588DC049E097EC9437017E7DDED0FEB,
