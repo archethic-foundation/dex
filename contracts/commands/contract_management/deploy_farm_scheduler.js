fc8c4bead9781659e006039a8d425eb0fb9cf9f2
bchamagne <bastien@archethic.net>
2024-08-01 12:36:26 +0200

    feat(command): deploy_farm_scheduler

---
+++ contracts/commands/contract_management/deploy_farm_scheduler.js
@@ -0,0 +1,94 @@
+import Archethic, { Utils } from "@archethicjs/sdk";
+import config from "../../config.js";
+import fs from "fs";
+import { getServiceGenesisAddress } from "../utils.js";
+
+const command = "deploy_farm_scheduler";
+const describe = "Deploy the farm' scheduler";
+const builder = {
+  access_seed: {
+    describe: "The Keychain access seed (default in env config)",
+    demandOption: false,
+    type: "string",
+  },
+  env: {
+    describe: "The environment config to use (default to local)",
+    demandOption: false,
+    type: "string",
+  },
+};
+
+const handler = async function (argv) {
+  const envName = argv["env"] ? argv["env"] : "local";
+  const env = config.environments[envName];
+
+  const keychainAccessSeed = argv["access_seed"]
+    ? argv["access_seed"]
+    : env.keychainAccessSeed;
+
+  if (keychainAccessSeed == undefined) {
+    console.log("Keychain access seed not defined");
+    process.exit(1);
+  }
+
+  const archethic = new Archethic(env.endpoint);
+  await archethic.connect();
+
+  let keychain;
+
+  try {
+    keychain = await archethic.account.getKeychain(keychainAccessSeed);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  const genesisAddress = getServiceGenesisAddress(keychain, "Farm-Scheduler");
+  console.log("Scheduler genesis address:", genesisAddress);
+
+  const contractCode = fs.readFileSync(
+    "./contracts/farm_lock_scheduler.exs",
+    "utf8",
+  );
+
+  const index = await archethic.transaction.getTransactionIndex(genesisAddress);
+
+  const storageNonce = await archethic.network.getStorageNoncePublicKey();
+  const { secret, authorizedPublicKeys } = keychain.ecEncryptServiceSeed(
+    "Farm-Scheduler",
+    [storageNonce],
+  );
+
+  let tx = archethic.transaction
+    .new()
+    .setType("contract")
+    .setCode(contractCode)
+    .addOwnership(secret, authorizedPublicKeys);
+
+  tx = keychain
+    .buildTransaction(tx, "Farm-Scheduler", index)
+    .originSign(Utils.originPrivateKey);
+
+  tx.on("sent", () => console.log("transaction sent !"))
+    .on("requiredConfirmation", async (_confirmations) => {
+      const txAddress = Utils.uint8ArrayToHex(updateTx.address);
+      console.log("Transaction validated !");
+      console.log("Address:", txAddress);
+      console.log(env.endpoint + "/explorer/transaction/" + txAddress);
+      process.exit(0);
+    })
+    .on("error", (context, reason) => {
+      console.log("Error while sending transaction");
+      console.log("Contest:", context);
+      console.log("Reason:", reason);
+      process.exit(1);
+    })
+    .send(50);
+};
+
+export default {
+  command,
+  describe,
+  builder,
+  handler,
+};
--- contracts/dex.js
+++ contracts/dex.js
@@ -1,53 +1,55 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-import init_keychain from './commands/contract_management/init_keychain.js'
-import deploy_factory from './commands/contract_management/deploy_factory.js'
-import deploy_router from './commands/contract_management/deploy_router.js'
-import update_router from './commands/contract_management/update_router.js'
-import update_pools from './commands/contract_management/update_pools.js'
-import update_farms from './commands/contract_management/update_farms.js'
-import update_protocol_fee from './commands/contract_management/update_protocol_fee.js'
-import update_lp_fee from './commands/contract_management/update_lp_fee.js'
-import update_farm_dates from './commands/contract_management/update_farm_dates.js'
+import init_keychain from "./commands/contract_management/init_keychain.js";
+import deploy_factory from "./commands/contract_management/deploy_factory.js";
+import deploy_farm_scheduler from "./commands/contract_management/deploy_farm_scheduler.js";
+import deploy_router from "./commands/contract_management/deploy_router.js";
+import update_router from "./commands/contract_management/update_router.js";
+import update_pools from "./commands/contract_management/update_pools.js";
+import update_farms from "./commands/contract_management/update_farms.js";
+import update_protocol_fee from "./commands/contract_management/update_protocol_fee.js";
+import update_lp_fee from "./commands/contract_management/update_lp_fee.js";
+import update_farm_dates from "./commands/contract_management/update_farm_dates.js";
 
-import create_user_wallet from './commands/test/create_user_wallet.js'
-import create_tokens from './commands/test/create_tokens.js'
-import deploy_pool from './commands/test/deploy_pool.js'
-import add_liquidity from './commands/test/add_liquidity.js'
-import remove_liquidity from './commands/test/remove_liquidity.js'
-import swap from './commands/test/swap.js'
+import create_user_wallet from "./commands/test/create_user_wallet.js";
+import create_tokens from "./commands/test/create_tokens.js";
+import deploy_pool from "./commands/test/deploy_pool.js";
+import add_liquidity from "./commands/test/add_liquidity.js";
+import remove_liquidity from "./commands/test/remove_liquidity.js";
+import swap from "./commands/test/swap.js";
 
-import deploy_farm from './commands/test/deploy_farm.js'
-import deposit from './commands/test/deposit.js'
-import claim from './commands/test/claim.js'
-import withdraw from './commands/test/withdraw.js'
+import deploy_farm from "./commands/test/deploy_farm.js";
+import deposit from "./commands/test/deposit.js";
+import claim from "./commands/test/claim.js";
+import withdraw from "./commands/test/withdraw.js";
 
-const y = yargs(hideBin(process.argv))
+const y = yargs(hideBin(process.argv));
 
-y.command(init_keychain).help()
-y.command(deploy_factory).help()
-y.command(deploy_router).help()
-y.command(update_router).help()
-y.command(update_pools).help()
-y.command(update_farms).help()
-y.command(update_protocol_fee).help()
-y.command(update_lp_fee).help()
-y.command(update_farm_dates).help()
+y.command(init_keychain).help();
+y.command(deploy_factory).help();
+y.command(deploy_farm_scheduler).help();
+y.command(deploy_router).help();
+y.command(update_router).help();
+y.command(update_pools).help();
+y.command(update_farms).help();
+y.command(update_protocol_fee).help();
+y.command(update_lp_fee).help();
+y.command(update_farm_dates).help();
 
-y.command(create_user_wallet).help()
+y.command(create_user_wallet).help();
 
-y.command(create_tokens).help()
-y.command(deploy_pool).help()
-y.command(add_liquidity).help()
-y.command(remove_liquidity).help()
-y.command(swap).help()
+y.command(create_tokens).help();
+y.command(deploy_pool).help();
+y.command(add_liquidity).help();
+y.command(remove_liquidity).help();
+y.command(swap).help();
 
-y.command(deploy_farm).help()
-y.command(deposit).help()
-y.command(claim).help()
-y.command(withdraw).help()
+y.command(deploy_farm).help();
+y.command(deposit).help();
+y.command(claim).help();
+y.command(withdraw).help();
 
-y.parse()
+y.parse();
