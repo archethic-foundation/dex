316896544c200422924123978514da75cb2d4d15
Chralu <contact@charly-caulet.net>
2024-09-30 11:49:05 +0200

    fix: :bug: DappClient closes connection when disposed.

--- lib/application/dapp_client.dart
+++ lib/application/dapp_client.dart
@@ -4,8 +4,12 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'dapp_client.g.dart';
 
 @riverpod
-Future<awc.ArchethicDAppClient> dappClient(DappClientRef ref) =>
-    awc.ArchethicDAppClient.auto(
-      origin: const awc.RequestOrigin(name: 'aeSwap'),
-      replyBaseUrl: '',
-    );
+Future<awc.ArchethicDAppClient> dappClient(DappClientRef ref) async {
+  final client = await awc.ArchethicDAppClient.auto(
+    origin: const awc.RequestOrigin(name: 'aeSwap'),
+    replyBaseUrl: '',
+  );
+  ref.onDispose(client.close);
+
+  return client;
+}
