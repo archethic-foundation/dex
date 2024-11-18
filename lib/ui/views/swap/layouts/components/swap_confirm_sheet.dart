89ec6509d92258469df913a6bc9dd949fafebe50
redDwarf03 <sylvain@archethic.net>
2024-11-18 12:51:38 +0100

    feat: :sparkles: Add a control to check if UCO balance is not too low after action validation

--- lib/ui/views/swap/layouts/components/swap_confirm_sheet.dart
+++ lib/ui/views/swap/layouts/components/swap_confirm_sheet.dart
@@ -1,12 +1,16 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
+// ignore_for_file: use_build_context_synchronously
+
 import 'dart:async';
 
 import 'package:aedex/ui/views/swap/bloc/provider.dart';
 import 'package:aedex/ui/views/swap/layouts/components/swap_confirm_infos.dart';
 import 'package:aedex/ui/views/swap/layouts/components/swap_in_progress_popup.dart';
+import 'package:aedex/ui/views/util/components/low_uco_warning_popup.dart';
 import 'package:aedex/ui/views/util/consent_uri.dart';
 import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart'
     as aedappfm;
+import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -73,6 +77,20 @@ class SwapConfirmSheetState extends ConsumerState<SwapConfirmSheet> {
             labelBtn: AppLocalizations.of(context)!.btn_confirm_swap,
             disabled: !consentChecked && swap.consentDateTime == null,
             onPressed: () async {
+              if (swap.tokenToSwap != null &&
+                  swap.tokenToSwap!.isUCO &&
+                  (Decimal.parse(
+                                swap.tokenToSwapBalance.toString(),
+                              ) -
+                              Decimal.parse(swap.tokenToSwapAmount.toString()))
+                          .toDouble() <
+                      kLowUCOWarningValue) {
+                final result = await LowUCOWarningPopup.getDialog(context);
+                if (result != null && result == false) {
+                  return;
+                }
+              }
+
               final swapNotifier = ref.read(swapFormNotifierProvider.notifier);
               unawaited(swapNotifier.swap(AppLocalizations.of(context)!));
               await SwapInProgressPopup.getDialog(
