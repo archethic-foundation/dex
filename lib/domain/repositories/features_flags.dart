7a51be8962ada6d359a9d8fe596ab239714dff21
redDwarf03 <sylvain@archethic.net>
2025-01-31 16:52:23 +0100

    Close maintenance

--- lib/domain/repositories/features_flags.dart
+++ lib/domain/repositories/features_flags.dart
@@ -1,6 +1,6 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 /// Simply activate/deactivate features.
 class FeatureFlags {
-  static const inMaintenanceGlobal = true;
-  static const inMaintenanceFarmLock = true;
+  static const inMaintenanceGlobal = false;
+  static const inMaintenanceFarmLock = false;
 }
